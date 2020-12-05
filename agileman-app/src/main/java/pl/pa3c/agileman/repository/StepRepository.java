263d2e2b1570cc009bd075c47f4086e4e57a8c61
patryk.klos <patryk.klos@external.t-systems.com>
2020-12-05 16:21:17 +0100

    Add steps to get task

--- agileman-app/src/main/java/pl/pa3c/agileman/repository/StepRepository.java
+++ agileman-app/src/main/java/pl/pa3c/agileman/repository/StepRepository.java
@@ -1,9 +1,13 @@
 package pl.pa3c.agileman.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.pa3c.agileman.model.task.Step;
 
 public interface StepRepository extends JpaRepository<Step, Long>{
+
+	List<Step> findAllByTaskId(Long id);
 
 }
--- agileman-app/src/main/java/pl/pa3c/agileman/service/TaskService.java
+++ agileman-app/src/main/java/pl/pa3c/agileman/service/TaskService.java
@@ -1,5 +1,8 @@
 package pl.pa3c.agileman.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +16,7 @@ import pl.pa3c.agileman.model.task.Task;
 import pl.pa3c.agileman.repository.StepRepository;
 
 @Service
-public class TaskService extends CommonService<Long, TaskSO, Task>{
+public class TaskService extends CommonService<Long, TaskSO, Task> {
 
 	@Autowired
 	private StepRepository stepRepository;
@@ -22,18 +25,27 @@ public class TaskService extends CommonService<Long, TaskSO, Task>{
 	public TaskService(JpaRepository<Task, Long> taskRepository) {
 		super(taskRepository);
 	}
-	
+
 	@Override
 	@Transactional
 	public TaskSO create(TaskSO entitySO) {
 		final TaskSO so = super.create(entitySO);
 
-		entitySO.getSteps().forEach(x->{
+		entitySO.getSteps().forEach(x -> {
 			x.setTaskId(so.getId());
 			Step step = stepRepository.save(mapper.map(x, Step.class));
 			so.getSteps().add(mapper.map(step, StepSO.class));
 		});
 		return so;
+	}
+
+	@Override
+	public TaskSO get(Long id) {
+		final TaskSO taskSO = super.get(id);
+		final List<StepSO> steps = stepRepository.findAllByTaskId(id).stream().map(x -> mapper.map(x, StepSO.class))
+				.collect(Collectors.toList());
+		taskSO.setSteps(steps);
+		return taskSO;
 	}
 
 }
