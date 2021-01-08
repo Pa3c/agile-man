d1de8faf080cbf46c7be36690585d1e9653fd434
patryk.klos <patryk.klos@external.t-systems.com>
2021-01-08 14:20:02 +0100

    Added task update notification

--- agileman-app/src/main/java/pl/pa3c/agileman/service/EmailConstant.java
+++ agileman-app/src/main/java/pl/pa3c/agileman/service/EmailConstant.java
@@ -10,7 +10,7 @@ public class EmailConstant {
     public static final String PASSWORD = "PA3C.agil3man";
     public static final String FROM_EMAIL = "task-service@agileman.com";
     public static final String CC_EMAIL = "";
-    public static final String EMAIL_SUBJECT = "Get Arrays, LLC - New Password";
+    public static final String EMAIL_SUBJECT = "Task changes";
     public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
     public static final String SMTP_HOST = "mail.smtp.host";
     public static final String SMTP_AUTH = "mail.smtp.auth";
@@ -18,4 +18,10 @@ public class EmailConstant {
     public static final int DEFAULT_PORT = 465;
     public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
     public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+    public static final String HTML_CONTENT = "text/html; charset=utf-8";
+    public static final String HTML_TABLE_START = "<table border=\"1\">";
+    public static final String HTML_HEADER_ROW = "<tr><th>Property Name</th><th>Old</th><th>New</th><tr>";
+    public static final String HTML_TABLE_END = "</table>";
+    public static final String HTML_EMAIL_BEGIN = "Information, <br> there where changes in the task that You observed <br>";
+    public static final String HTML_EMAIL_FOOTER = "<br>-------<br>The Notification Service.";
 }
--- agileman-app/src/main/java/pl/pa3c/agileman/service/TaskNotificationService.java
+++ agileman-app/src/main/java/pl/pa3c/agileman/service/TaskNotificationService.java
@@ -1,18 +1,7 @@
 package pl.pa3c.agileman.service;
 
 import static javax.mail.Message.RecipientType.TO;
-import static pl.pa3c.agileman.service.EmailConstant.DEFAULT_PORT;
-import static pl.pa3c.agileman.service.EmailConstant.EMAIL_SUBJECT;
-import static pl.pa3c.agileman.service.EmailConstant.FROM_EMAIL;
-import static pl.pa3c.agileman.service.EmailConstant.GMAIL_SMTP_SERVER;
-import static pl.pa3c.agileman.service.EmailConstant.PASSWORD;
-import static pl.pa3c.agileman.service.EmailConstant.SIMPLE_MAIL_TRANSFER_PROTOCOL;
-import static pl.pa3c.agileman.service.EmailConstant.SMTP_AUTH;
-import static pl.pa3c.agileman.service.EmailConstant.SMTP_HOST;
-import static pl.pa3c.agileman.service.EmailConstant.SMTP_PORT;
-import static pl.pa3c.agileman.service.EmailConstant.SMTP_STARTTLS_ENABLE;
-import static pl.pa3c.agileman.service.EmailConstant.SMTP_STARTTLS_REQUIRED;
-import static pl.pa3c.agileman.service.EmailConstant.USERNAME;
+import static pl.pa3c.agileman.service.EmailConstant.*;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -31,6 +20,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -53,38 +43,43 @@ public class TaskNotificationService {
 	private final StringBuilder sb = new StringBuilder();
 
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
-	public void afterTransaction(TaskUpdateEvent event)
-			throws IllegalAccessException, MessagingException {
+	@Async
+	public void afterTransaction(TaskUpdateEvent event) throws IllegalAccessException, MessagingException {
 
 		final List<String> emails = userTaskRepository
 				.findAllByTaskIdAndTypeIn(event.getNewState().getId(), Arrays.asList(Type.OBSERVER)).stream()
 				.map(x -> x.getUser().getEmail()).collect(Collectors.toList());
-		
-		if(emails.isEmpty()) {
+
+		if (emails.isEmpty()) {
 			return;
 		}
 
 		final Map<String, List<String>> taskDifferences = findTaskDifferences(event.getOldState(), event.getNewState());
-
-		String content = buildContent(taskDifferences);
+		final String content = buildHTMLContent(event.getOldState(), taskDifferences);
+		
 		sendNotificationEmail(content, emails);
-
 	}
 
-	public String buildContent(Map<String, List<String>> changes) {
+	public String buildHTMLContent(Task task, Map<String, List<String>> changes) {
 		sb.setLength(0);
 
-		changes.forEach((key, value) -> {
-			sb.append("\n" + key + " old = " + value.get(0) + " new = " + value.get(1) + "\n");
-		});
+		sb.append(HTML_EMAIL_BEGIN);
+		sb.append("<h5>");
+		sb.append(task.getTitle());
+		sb.append("</h5><br>");
+		sb.append(HTML_TABLE_START);
+		sb.append(HTML_HEADER_ROW);
+		changes.forEach((key, value) -> sb.append("<tr><td>").append(splitCamelCase(key)).append("</td><td>")
+				.append(value.get(0)).append("</td><td>").append(value.get(1)).append("</td><tr>"));
+		sb.append(HTML_TABLE_END).append(HTML_EMAIL_FOOTER);
 
 		return sb.toString();
-
 	}
 
 	public void sendNotificationEmail(String text, List<String> emails) throws MessagingException {
 		final Message message = createEmail(text, buildAddressesString(emails));
-		final SMTPTransport smtpTransport = (SMTPTransport) getEmailSession().getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
+		final SMTPTransport smtpTransport = (SMTPTransport) getEmailSession()
+				.getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
 		smtpTransport.connect(GMAIL_SMTP_SERVER, USERNAME, PASSWORD);
 		smtpTransport.sendMessage(message, message.getAllRecipients());
 		smtpTransport.close();
@@ -100,20 +95,18 @@ public class TaskNotificationService {
 		return sb.toString().substring(0, sb.length() - 1);
 	}
 
-	private Message createEmail(String text, String addresses) throws MessagingException {
+	private Message createEmail(String htmlContent, String addresses) throws MessagingException {
 		final Message message = new MimeMessage(getEmailSession());
 		message.setFrom(new InternetAddress(FROM_EMAIL));
 		message.setRecipients(TO, InternetAddress.parse(addresses));
 		message.setSubject(EMAIL_SUBJECT);
-		message.setText("Information" + ", \n \n there where changes in the task that You observed \n \n" + text
-				+ "\n \n The Notification Service");
+		message.setContent(htmlContent, HTML_CONTENT);
 		message.setSentDate(new Date());
 		message.saveChanges();
 		return message;
 	}
 
-	private Map<String, List<String>> findTaskDifferences(Task oldTask, Task newTask)
-			throws IllegalArgumentException, IllegalAccessException {
+	private Map<String, List<String>> findTaskDifferences(Task oldTask, Task newTask) throws IllegalAccessException {
 
 		final Map<String, List<String>> notMatchProperties = new HashMap<>();
 
@@ -157,6 +150,11 @@ public class TaskNotificationService {
 		properties.put(SMTP_STARTTLS_ENABLE, true);
 		properties.put(SMTP_STARTTLS_REQUIRED, true);
 		return Session.getInstance(properties, null);
+	}
+
+	static String splitCamelCase(String s) {
+		return s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])",
+				"(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
 	}
 
 }
